e9be765155a83696c173b3d1e9ca01ef2a36e5ca
juanfmolina <jufermoflo134@hotmail.com>
2014-10-22 19:14:01 -0500

    Agregando actividad para mapa y menú

--- src/co/edu/udea/maps/MapsActivity.java
+++ src/co/edu/udea/maps/MapsActivity.java
@@ -1,68 +1,72 @@
 package co.edu.udea.maps;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
 public class MapsActivity extends Activity {
 
 	private GoogleMap googleMap;
-	 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
- 
-        try {
-            // Loading map
-            initilizeMap();
- 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
- 
-    }
- 
-    /**
-     * function to load map. If map is not created it will create it for you
-     * */
-    private void initilizeMap() {
-        if (googleMap == null) {
-            googleMap = ((MapFragment)getFragmentManager().findFragmentById(
-                    R.id.map)).getMap();
- 
-            // check if map is created successfully or not
-            if (googleMap == null) {
-                Toast.makeText(getApplicationContext(),
-                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
-                        .show();
-            }else{
-            	googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-            	// latitude and longitude
-            	double latitude = 6.267588;
-            	double longitude = -75.567722;
-            	 
-            	// create marker
-            	MarkerOptions marker = new MarkerOptions().position(new LatLng(latitude, longitude)).title("Hello Maps ");
-            	 
-            	// adding marker
-            	googleMap.addMarker(marker);
-            }
-        }
-    }
- 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        initilizeMap();
-    }
- 
-}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_maps);
 
+		try {
+			// Loading map
+			initilizeMap();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public void setNormalType(View view){
+		
+		googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+	}
+	public void setHybridType(View view){
+		googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+		
+	}
+	public void setSatelliteType(View view){
+		googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+	}
+	public void setTerrainType(View view){
+		googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+	}
+
+	/**
+	 * function to load map. If map is not created it will create it for you
+	 * */
+	private void initilizeMap() {
+		
+		if (googleMap == null) {
+			googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+					R.id.map)).getMap();
+
+			// check if map is created successfully or not
+			if (googleMap == null) {
+				Toast.makeText(getApplicationContext(),
+						"Sorry! unable to create maps", Toast.LENGTH_SHORT)
+						.show();
+			} else {
+				googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+							
+			}
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		initilizeMap();
+	}
+
+}
